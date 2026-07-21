a4f69654c75d83ee283c539ee9208510e5865dad
Jung Sun A <amber0809@naver.com>
2026-07-21 13:22:28 +0900

    style: arbitrary color 값 토큰으로 변경

--- apps/web/src/app/tournament/[id]/match/_components/RoundTransition.tsx
+++ apps/web/src/app/tournament/[id]/match/_components/RoundTransition.tsx
@@ -49,7 +49,7 @@ function RoundTransition({
   const isFinal = stage === 'toFinal';
 
   const containerClassName = isFinal
-    ? 'bg-gradient-to-b from-[#ECF8FE] via-[#F5FCFF] to-white'
+    ? 'bg-gradient-to-b from-sky-blue-50 via-[#F5FCFF] to-white'
     : 'bg-bg-layer-basement';
 
   return (
