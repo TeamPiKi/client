31ad8e0509a2d457bfdecd49d467143febc179dc
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2026-08-02 00:53:11 +0900

    feat: 토너먼트 item status ready일 때도 수정 가능하도록 변경

--- apps/web/src/app/tournament/[id]/item/[itemId]/_apis/patchTournamentItem.ts
+++ apps/web/src/app/tournament/[id]/item/[itemId]/_apis/patchTournamentItem.ts
@@ -2,17 +2,13 @@ import { clientApi } from '@/apis/client';
 import { ENDPOINTS } from '@/consts/api';
 import type { ApiResponseT } from '@/types/api';
 
-import type { PatchTournamentItemResponseT } from '../_types/tournamentItem';
-
 export const patchTournamentItem = async (
   tournamentId: number,
   tournamentItemId: number,
   formData: FormData
 ) => {
-  const { data } = await clientApi.patch<ApiResponseT<PatchTournamentItemResponseT>>(
+  await clientApi.patch<ApiResponseT<null>>(
     ENDPOINTS.TOURNAMENT_ITEM(tournamentId, tournamentItemId),
     formData
   );
-
-  return data.data;
 };
