8fdcb01664564f6fa0b8409290dc1ec44f2fba02
Jung Sun A <amber0809@naver.com>
2026-08-04 14:52:07 +0900

    style: 홈 토너먼트 초대 코드 유효하지 않은 경우 뜨는 모달 스타일 오류 수정

--- apps/web/src/app/home/_components/invite-code-dialog/InvalidCodeDialog.tsx
+++ apps/web/src/app/home/_components/invite-code-dialog/InvalidCodeDialog.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Dialog, DialogContent, DialogDescription, DialogTitle } from '@/components/dialog';
+import Spacing from '@/components/spacing';
 
 import SadFace from '../../_assets/sad-face.svg';
 
@@ -14,19 +15,24 @@ function InvalidCodeDialog({ open, onOpenChange }: InvalidCodeDialogProps) {
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent showCloseButton={false} className="flex flex-col items-center gap-5 p-6">
-        <SadFace className="size-7.75" aria-hidden />
+      <DialogContent showCloseButton={false} className="flex flex-col items-center p-5">
+        <SadFace className="size-7.75 text-icon-accent" aria-hidden />
 
-        <div className="flex flex-col items-center gap-1">
-          <DialogTitle className="text-center heading-1-bold text-text-neutral-primary">
-            코드가 유효하지 않아요
-          </DialogTitle>
-          <DialogDescription className="text-center body-2-medium text-text-neutral-tertiary">
-            입력한 코드와 일치하는 토너먼트가 없어요.
-            <br />
-            코드를 다시 확인해주세요.
-          </DialogDescription>
-        </div>
+        <Spacing size={12} />
+
+        <DialogTitle className="text-center heading-1-bold text-text-neutral-primary">
+          코드가 유효하지 않아요
+        </DialogTitle>
+
+        <Spacing size={8} />
+
+        <DialogDescription className="text-center body-2-medium text-text-neutral-tertiary">
+          입력한 코드와 일치하는 토너먼트가 없어요.
+          <br />
+          코드를 다시 확인해주세요.
+        </DialogDescription>
+
+        <Spacing size={16} />
 
         <button
           type="button"
