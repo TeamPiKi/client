c6700306f106d21e27e7782734dc9c409b91e1c4
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2026-08-11 21:57:49 +0900

    fix: 빈 메모로 저장해도 메모가 지워지지 않던 문제 수정

--- apps/web/src/app/archive/wish/[id]/_hooks/usePatchWish.ts
+++ apps/web/src/app/archive/wish/[id]/_hooks/usePatchWish.ts
@@ -16,10 +16,10 @@ export const usePatchWish = (wishId: number) => {
   const { mutate: patchWishMutation, isPending: isPatchWishPending } = useMutation({
     mutationFn: (body: PatchWishRequestT) => {
       const formData = new FormData();
-      if (body.name) formData.append('name', body.name);
-      if (body.price) formData.append('price', String(body.price));
+      if (typeof body.name === 'string') formData.append('name', body.name);
+      if (typeof body.price === 'number') formData.append('price', String(body.price));
       if (body.image) formData.append('image', body.image);
-      if (body.memo) formData.append('memo', body.memo);
+      if (typeof body.memo === 'string') formData.append('memo', body.memo);
       return patchWish(wishId, formData);
     },
     onSuccess: () => {
