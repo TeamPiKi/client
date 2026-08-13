f8e4203b7f0dfb4ac43bee010a7b969878bacf6c
Jung Sun A <amber0809@naver.com>
2026-08-13 11:36:19 +0900

    style: 알림 페이지 루트를 main 태그로 변경

--- apps/web/src/app/notification/page.tsx
+++ apps/web/src/app/notification/page.tsx
@@ -4,7 +4,7 @@ import NotificationContent from './_components/NotificationContent';
 
 function Notification() {
   return (
-    <div className="flex h-dvh flex-col bg-gray-50 px-5 pt-padding-top">
+    <main className="flex h-dvh flex-col bg-gray-50 px-5 pt-padding-top">
       <Header
         left={<HeaderIcon name="BACK" />}
         center="알림 히스토리"
@@ -14,7 +14,7 @@ function Notification() {
       <div className="mt-4 hide-scrollbar flex-1 overflow-y-auto">
         <NotificationContent />
       </div>
-    </div>
+    </main>
   );
 }
 
