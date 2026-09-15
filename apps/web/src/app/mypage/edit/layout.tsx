5b49dfc00be0512ed6d6e01ea8d5cd7016eeb034
Jung Sun A <amber0809@naver.com>
2026-09-15 15:52:57 +0900

    feat: 프로필 수정 게스트 접근 마이페이지 리다이렉트 전환

--- apps/web/src/app/mypage/edit/layout.tsx
+++ apps/web/src/app/mypage/edit/layout.tsx
@@ -1,5 +1,5 @@
-import LoginRequired from '@/components/common/login-required';
-import { LOGIN_REQUIRED_TITLE } from '@/components/common/login-required/loginRequired.const';
+import { redirect } from 'next/navigation';
+
 import { ROUTES } from '@/consts/route';
 import { getRoleOrRedirect } from '@/utils/getRoleOrRedirect';
 
@@ -10,11 +10,7 @@ type MypageEditLayoutProps = {
 async function MypageEditLayout({ children }: MypageEditLayoutProps) {
   const role = await getRoleOrRedirect();
 
-  // TODO: 멤버 아닌 경우 마이페이지로 리다이렉트되도록 변경 필요
-  if (role !== 'MEMBER')
-    return (
-      <LoginRequired title={LOGIN_REQUIRED_TITLE.MYPAGE_EDIT} redirectPath={ROUTES.MYPAGE_EDIT} />
-    );
+  if (role !== 'MEMBER') redirect(ROUTES.MYPAGE);
 
   return children;
 }
