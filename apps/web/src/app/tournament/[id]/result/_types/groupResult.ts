686e10bdfb26e9b53c8dc1135f2b86b43a7cbdcc
Jung Sun A <amber0809@naver.com>
2026-09-17 13:35:54 +0900

    fix: 전체 결과 게스트 마스킹 및 로그인 유도 되돌리기 (#662)

--- apps/web/src/app/tournament/[id]/result/_types/groupResult.ts
+++ apps/web/src/app/tournament/[id]/result/_types/groupResult.ts
@@ -1,13 +1,9 @@
-type GroupResultParticipantBaseT = {
+type GroupResultParticipantT = {
+  userId: string;
   nickname: string;
   profileImage: string;
-  isWithdrawn: boolean;
   isHost: boolean;
 };
-
-export type GroupResultParticipantT =
-  | (GroupResultParticipantBaseT & { isMasked: false; userId: string })
-  | (GroupResultParticipantBaseT & { isMasked: true; userId: null });
 
 export type GroupResultItemT = {
   rank: number;
